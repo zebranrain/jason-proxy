c2cd7b223662fd9edd3d5f9a9ea0ee5c6229e865
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-02 17:12:27 -0700

    Preparing for AWS deployment

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(morgan('dev'));
 app.use('/:ticker', express.static('./public'));
